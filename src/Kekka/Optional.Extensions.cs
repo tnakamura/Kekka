54d9138503f55bd59c191e990b9d64de3a3443fe
tnakamura <griefworker@gmail.com>
2024-11-29 16:15:11 +0900

    Add extension methods for Optional

---
+++ src/Kekka/Optional.Extensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Kekka;
+
+public static partial class Optional
+{
+    public static Optional<T2> Select<T1, T2>(this Optional<T1> optional, Func<T1, T2> selector)
+        where T1 : notnull
+        where T2 : notnull
+    {
+        if (optional.TryGet(out var value))
+        {
+            return new Optional<T2>(selector(value));
+        }
+        else
+        {
+            return Optional<T2>.None;
+        }
+    }
+
+    public static Optional<T2> SelectMany<T1, T2>(
+        this in Optional<T1> optional,
+        Func<T1, Optional<T2>> selector)
+        where T1 : notnull
+        where T2 : notnull
+    {
+        if (optional.TryGet(out var value))
+        {
+            return selector(value);
+        }
+        else
+        {
+            return Optional<T2>.None;
+        }
+    }
+
+    public static Optional<T2> SelectMany<T1, TCollection, T2>(
+        this Optional<T1> source,
+        Func<T1, Optional<TCollection>> selector,
+        Func<T1, TCollection, T2> resultSelector)
+        where T1 : notnull
+        where T2 : notnull
+        where TCollection : notnull
+    {
+        if (source.TryGet(out var value1))
+        {
+            var result1 = selector(value1);
+            if (result1.TryGet(out var value2))
+            {
+                var result2 = resultSelector(value1, value2);
+                return new Optional<T2>(result2);
+            }
+            else
+            {
+                return Optional<T2>.None;
+            }
+        }
+        else
+        {
+            return Optional<T2>.None;
+        }
+    }
+}
+
---
+++ test/Kekka.Tests/OptionalTest.cs
@@ -0,0 +1,32 @@
+﻿namespace Kekka.Tests;
+
+public class OptionalTest
+{
+    [Fact]
+    public void TryGetTest_when_has_value()
+    {
+        var actual = from x in new Optional<string>("A")
+                     from y in new Optional<string>("B")
+                     from z in new Optional<string>("C")
+                     select x + y + z;
+        if (actual.TryGet(out var value))
+        {
+            Assert.Equal("ABC", value);
+        }
+        else
+        {
+            Assert.Fail();
+        }
+    }
+
+    [Fact]
+    public void TryGet_when_none()
+    {
+        var actual = from x in new Optional<string>("A")
+                     from y in Optional<string>.None
+                     from z in new Optional<string>("C")
+                     select x + y + z;
+        Assert.False(actual.HasValue);
+        Assert.False(actual.TryGet(out var value));
+    }
+}
